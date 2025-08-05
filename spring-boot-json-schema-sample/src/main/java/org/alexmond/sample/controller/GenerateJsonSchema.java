6eef664454ba5d5781213bf7a11e7c2904e1889a
alexmond <alexmond@gmail.com>
2025-08-04 22:32:21 -0400

    Remove redundant dependencies, clean up `application.yaml`, consolidate metadata in `pom.xml`, and simplify `GenerateJsonSchema` controller logic.

--- spring-boot-json-schema-sample/src/main/java/org/alexmond/sample/controller/GenerateJsonSchema.java
+++ spring-boot-json-schema-sample/src/main/java/org/alexmond/sample/controller/GenerateJsonSchema.java
@@ -5,35 +5,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.alexmond.config.json.schema.metaextension.BootConfigMetaLoader;
 import org.alexmond.config.json.schema.service.JsonSchemaService;
 import org.alexmond.config.json.schema.service.MissingTypeCollector;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.Paths;
-import java.util.*;
 
 @RestController
 @Slf4j
 public class GenerateJsonSchema {
 
     @Autowired
-    private ApplicationContext context;
-
-
-    @Autowired
     private JsonSchemaService jsonSchemaService;
 
     @Autowired
-    private BootConfigMetaLoader bootConfigMetaLoader;
-
-    @Autowired
     private MissingTypeCollector missingTypeCollector;
-
-    private final Map<String, Object> mappingsMap = new HashMap<>();
 
     @GetMapping("/configSchema")
     public String Config() throws Exception {
@@ -54,9 +42,7 @@ public class GenerateJsonSchema {
         log.info("Writing yaml schema");
         yamlWriter.writeValue(Paths.get("sample-schema.yaml").toFile(), jsonMapper.readTree(jsonConfigSchema));
 
-        missingTypeCollector.getMissingTypes().forEach(type -> {
-            log.info("Missing type: {}",type);
-        });
+        missingTypeCollector.getMissingTypes().forEach(type -> log.info("Missing type: {}",type));
 
         return "configSample";
     }
